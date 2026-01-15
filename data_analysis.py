cfab9d8669f8e241da1ea30bd991c23f169ced1a
Kiwiwentbad <Nikiyawoodard@icloud.com>
2026-01-15 19:16:16 +0000

    very basic data analysis

--- data_analysis.py
+++ data_analysis.py
@@ -1,1 +1,21 @@
+# Nikiya Woodard
 
+import pandas as pd
+
+# Read the csv file into a variable
+df = pd.read_csv("Marvel_Comics.csv")
+
+# Display first few rows in the dataset
+print(df.head())
+
+# Put active_years into a variable
+year_count = df["active_years"]
+
+# Display largest year count
+#print(year_count.max())
+
+largest_year_count = df[df['active_years'] == df['active_years'].max()]
+print("Largest year of comic books collected")
+
+# Display smallest year count
+#print(year_count.min())
